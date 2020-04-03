5ee7575fe4dbb528336db38ca2d33b3ce835da64
Alicia Gallagher <algallagher@deloitte.com>
2020-04-03 14:48:10 -0400

    add users is working

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -3,14 +3,46 @@
 # Exit if an error is encountered
 set -e
 
+### Set defaults for any of the environment variables
+set_defaults() {
+    if [[ ! "${MODE}" ]]; then
+        MODE="STANDALONE"
+    fi
+
+    if [[ ! "${JDG_USERNAME}" ]]; then
+        JDG_USERNAME="myuser"
+    fi
+
+    if [[ ! "${JDG_PASSWORD}" ]]; then
+        JDG_PASSWORD="admin123!"
+    fi
+}
+
 ### Create  User
 create_user() {
-    echo "placeholder"
+     ${JDG_HOME}/jboss-datagrid-7.3.0-server/bin/add-user.sh -a -u ${JDG_USERNAME} -p ${JDG_PASSWORD} -ro supervisor,reader,writer --silent
 }
+
 
 start_standalone() {
 
-    ${JDG_HOME}/jboss-datagrid-7.3.0-server/bin/standalone.sh
+    ${JDG_HOME}/jboss-datagrid-7.3.0-server/bin/standalone.sh -c standalone.xml
 }
 
-start_standalone
+set_defaults
+#check_env_values
+create_user
+
+case "${MODE}" in
+    STANDALONE*)
+        echo "Starting JBOSS Data Grid Server in STANDALONE mode"
+        start_standalone
+    ;;
+    #DOMAIN_MASTER*)
+    #    echo "Starting JBOSS Data Grid Server in DOMAIN mode as Domain Master"
+    #    start_domain_master
+    #;;
+    #DOMAIN_SLAVE*)
+    #    echo "Starting JBOSS Data Grid Server in DOMAIN mode as Domain Slave"
+    #    start_domain_master
+esac
