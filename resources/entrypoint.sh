205fad9727bdce3c40a4bc1297249994194a1401
Alicia Gallagher <algallagher@deloitte.com>
2020-04-07 17:48:39 -0400

    add the bind address for startup

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -10,7 +10,7 @@ set_defaults() {
     fi
 
     if [[ ! "${JDG_USERNAME}" ]]; then
-        JDG_USERNAME="myuser"
+        JDG_USERNAME="admin"
     fi
 
     if [[ ! "${JDG_PASSWORD}" ]]; then
@@ -18,15 +18,32 @@ set_defaults() {
     fi
 }
 
+# Creates the options to be passed to the program that will start up jboss, facilitating variable replacement in the
+# XML files
+create_option_string() {
+    OPTS="$OPTS"
+    
+    # set the host ip for eth0 - this may not scale well
+    HOST_IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+
+    OPTS="${OPTS} -Djboss.bind.address=${HOST_IP}"
+    OPTS="${OPTS} -Djboss.bind.address.unsecure=${HOST_IP}"
+    OPTS="${OPTS} -Djboss.bind.address.management=${HOST_IP}"
+
+    echo "Created Option String: ${OPTS}"
+}
+
 ### Create  User
 create_user() {
-     ${JDG_HOME}/jboss-datagrid-7.3.1-server/bin/add-user.sh -a -u ${JDG_USERNAME} -p ${JDG_PASSWORD} -ro supervisor,reader,writer --silent
+     ${JDG_HOME}/jboss-datagrid-7.3.1-server/bin/add-user.sh -a -u ${JDG_USERNAME} -p ${JDG_PASSWORD} -ro admin --silent
 }
 
 
 start_standalone() {
 
-    ${JDG_HOME}/jboss-datagrid-7.3.1-server/bin/standalone.sh -c standalone.xml
+    create_option_string
+
+    ${JDG_HOME}/jboss-datagrid-7.3.1-server/bin/standalone.sh -c standalone.xml ${OPTS}
 }
 
 set_defaults
